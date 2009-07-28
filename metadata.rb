44db499cabc1705edd468aad157146c1b446b621
Jim Van Fleet <jim@jimvanfleet.com>
2009-07-28 17:52:26 -0400

    Slight shift to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
+maintainer       "Jim Van Fleet"
+maintainer_email "jim@jimvanfleet.com"
+license          "MIT"
 name    "database"
 description 'Additional setup on top of mysql::server to ensure security and availability to apps'
 depends "mysql::server"
 depends "apt"
 depends "gems"
-maintainer 'Jim Van Fleet'
-maintainer_email 'jvanfleet@tradeking.com'
